17275b23cf0dc0f81b4d0592a7a4cd64edf5369c
KrystianBetka <k.betka.718@studms.ug.edu.pl>
2023-01-19 21:37:54 +0100

    mern

---
+++ routes/record.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const recordRoutes = express.Router();
+const dbo = require("../db/conn");
+const ObjectId = require("mongodb").ObjectId;
+
+recordRoutes.route("/products").get(function (req, res) {
+    let db_connect = dbo.getDb("mern");
+    db_connect
+        .collection("products")
+        .find({})
+        .toArray(function (err, result) {
+            if (err) throw err;
+            res.json(result);
+        });
+});
+
+
+module.exports = recordRoutes;
