fd31dbbd9c0c7ad11c351b65342d9d9b646e07e5
KrystianBetka <k.betka.718@studms.ug.edu.pl>
2023-01-29 15:28:12 +0100

    docker

--- db/conn.js
+++ db/conn.js
@@ -14,7 +14,7 @@ module.exports = {
             
             if (db)
             {
-                _db = db.db("mern");
+                _db = db.db("products");
                 console.log("Successfully connected to MongoDB.");
             }
             return callback(err);
--- routes/record.js
+++ routes/record.js
@@ -3,16 +3,65 @@ const recordRoutes = express.Router();
 const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
 
-recordRoutes.route("/products").get(function (req, res) {
-    let db_connect = dbo.getDb("mern");
-    db_connect
-        .collection("products")
-        .find({})
-        .toArray(function (err, result) {
-            if (err) throw err;
-            res.json(result);
-        });
+recordRoutes.route("/products").get(function(req, res) {
+    let db_connect = dbo.getDb("products");
+    db_connect.collection("products").find({}).toArray(function(err, result) {
+        if (err) throw err;
+        res.json(result);
+    });
 });
+
+recordRoutes.route("/products/:id").get(function(req, res) {
+    let db_connect = dbo.getDb("products");
+    let myquery = {_id: ObjectId(req.params.id)};
+    db_connect.collection("products").findOne(myquery, function(err, result) {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+
+recordRoutes.route("/products/add").post(function(req, response){
+    let db_connect = dbo.getDb("products");
+    let myobj = {
+        name: req.body.name,
+        position: req.body.position,
+        level: req.body.level
+    };
+    db_connect.collection("products").insertOne(myobj, function(err, res){
+        if (err) throw err;
+        response.json(res);
+    });
+});
+
+recordRoutes.route("/update/:id").post(function(req, response){
+    let db_connect = dbo.getDb("products");
+    let myquery = {_id: ObjectId(req.params.id)};
+    let newValues = {
+        $set: {
+            name: req.body.name,
+            position: req.body.position,
+            level:  req.body.level
+        },
+    };
+    db_connect.collection("products").updateOne(myquery, newValues, function(err, res){
+        if (err) throw err;
+        console.log("1 document updated successfully");
+        response.json(res);
+    });
+});
+
+recordRoutes.route("/:id").delete(function (req, res) {
+    let db_connect = dbo.getDb("products");
+    let myquery = {_id: ObjectId(req.params.id)};
+    db_connect.collection("products").deleteOne(myquery, function(err, obj){
+        if (err) throw err;
+        console.log("1 document deleted");
+        res.json(obj);
+    });
+})
+
+module.exports = recordRoutes;
+
 
 
 module.exports = recordRoutes;
